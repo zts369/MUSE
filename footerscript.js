0df862b8868953a0118702e1393e426078e597fb
jasonbenedlct <jason_benedict_lee@dlsu.edu.ph>
2026-02-16 23:34:54 +0800

    Fixed footer settings for all html files

--- footerscript.js
+++ footerscript.js
@@ -1,14 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-    
-    // Load Navbar
-    fetch('navbar.html')
-        .then(response => response.text())
-        .then(data => {
-            const nav = document.getElementById('navbar-placeholder');
-            if(nav) nav.innerHTML = data;
-        });
 
-    // Load Footer
     fetch('footer.html')
         .then(response => response.text())
         .then(data => {
